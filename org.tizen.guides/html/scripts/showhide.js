0a20d25c78684fd0424113b324809f6cdf0dd324
geunhui.lim <geunhui.lim@samsung.com>
2015-05-14 15:46:13 +0900

    [js script] layout modification script is modified(about toc-modification)
    Change-Id: I0000000000000000000012430000565600000000
    Signed-off-by: geunhui.lim <geunhui.lim@samsung.com>

--- org.tizen.guides/html/scripts/showhide.js
+++ org.tizen.guides/html/scripts/showhide.js
@@ -131,7 +131,7 @@ $(document).ready(function(){
 		}
 		$('#contents').css('padding-top', $('.help_breadcrumbs').outerHeight()*(7/6));
 		$('a.top').css('bottom', $('#footer').outerHeight());
-		$('a.top').css('left', $('#navigation').position()['left'] + (($('#navigation').outerWidth() - $('a.top').outerWidth())/2));
+		$('a.top').css('left', $('#toc-navigation').position()['left'] + (($('#toc-navigation').outerWidth() - $('a.top').outerWidth())/2));
 
 		$('#toc').css('top', $('#toc_border').position()['top'] + 7);
 
@@ -147,7 +147,7 @@ $(document).ready(function(){
 		$('#container #contents').css('margin-bottom', $('#footer').outerHeight());
 		//$('#toc').css('top', $('#toc_border').position()['top'] + 2);
 		$('#toc').css('top', $('#toc_border').position()['top'] + 7);
-		$('#contents').css('right', $(window).width() - $('#navigation').position()['left'] + 5);
+		$('#contents').css('right', $(window).width() - $('#toc-navigation').position()['left'] + 5);
 		//$('#toc').css('width', $('#toc_border').width() - 40);
 		$('#toc').css('width', $('#toc_border').width() - 49);
 	};
